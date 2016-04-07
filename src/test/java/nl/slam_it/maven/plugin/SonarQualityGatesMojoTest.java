72cef0421ce30242db7e24a520dd06efdba817a1
Sander Lammertink <sander.lammertink@gmail.com>
2016-04-07 11:43:58 +0200

    Added test case for sonar project key pom property Added assertions on the resource query parameter

--- src/test/java/nl/slam_it/maven/plugin/SonarQualityGatesMojoTest.java
+++ src/test/java/nl/slam_it/maven/plugin/SonarQualityGatesMojoTest.java
@@ -1,14 +1,20 @@
 package nl.slam_it.maven.plugin;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.resources.TestResources;
+import org.apache.maven.project.MavenProject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -31,6 +37,7 @@ public class SonarQualityGatesMojoTest {
     private HttpServer server;
     private SonarEventHandler sonarEventHandler;
     private Mojo mojo;
+    private MavenProject project;
 
     @Before
     public void setUp() throws Exception {
@@ -41,7 +48,8 @@ public class SonarQualityGatesMojoTest {
         server.setExecutor(null);
         server.start();
 
-        mojo = rule.lookupConfiguredMojo(rule.readMavenProject(resources.getBasedir("")), "inspect");
+        project = rule.readMavenProject(resources.getBasedir(""));
+        mojo = rule.lookupConfiguredMojo(project, "inspect");
     }
 
     @After
@@ -54,9 +62,12 @@ public class SonarQualityGatesMojoTest {
         exception.expect(MojoExecutionException.class);
         exception.expectMessage("Critical issues != 0, Coverage < 75");
 
-        sonarEventHandler.setResponse(200, getResponse("red.json"));
-
-        mojo.execute();
+        try {
+            sonarEventHandler.setResponse(200, getResponse("red.json"));
+            mojo.execute();
+        } finally {
+            assertThat(sonarEventHandler.getResource(), is("nl.slam-it.maven:test-sonar-quality-gates-maven-plugin"));
+        }
     }
 
     @Test
@@ -64,11 +75,24 @@ public class SonarQualityGatesMojoTest {
         sonarEventHandler.setResponse(200, getResponse("recovered.json"));
 
         mojo.execute();
+
+        assertThat(sonarEventHandler.getResource(), is("nl.slam-it.maven:test-sonar-quality-gates-maven-plugin"));
     }
 
     @Test
-    public void noEvents() throws Exception {
+    public void noEvents() throws MojoFailureException, MojoExecutionException {
         mojo.execute();
+
+        assertThat(sonarEventHandler.getResource(), is("nl.slam-it.maven:test-sonar-quality-gates-maven-plugin"));
+    }
+
+    @Test
+    public void sonarProjectKeyProperty() throws MojoFailureException, MojoExecutionException {
+        project.getProperties().put("sonar.projectKey", "nl.slam-it.maven:test-property");
+
+        mojo.execute();
+
+        assertThat(sonarEventHandler.getResource(), is("nl.slam-it.maven:test-property"));
     }
 
     @Test
@@ -77,9 +101,12 @@ public class SonarQualityGatesMojoTest {
         exception.expectMessage(
             "Sonar responded with an error message: Resource not found: nl.slam-it.foo:no-existing-project");
 
-        sonarEventHandler.setResponse(404, getResponse("error.json"));
-
-        mojo.execute();
+        try {
+            sonarEventHandler.setResponse(404, getResponse("error.json"));
+            mojo.execute();
+        } finally {
+            assertThat(sonarEventHandler.getResource(), is("nl.slam-it.maven:test-sonar-quality-gates-maven-plugin"));
+        }
     }
 
     @Test
@@ -105,23 +132,38 @@ public class SonarQualityGatesMojoTest {
     }
 
     private static final class SonarEventHandler implements HttpHandler {
+        private static final Pattern RESOURCE_PATTERN = Pattern.compile("^.*resource=(.*)$");
 
         private String response = "[]";
+        private String resource = "";
         private int status = 200;
 
         public void handle(final HttpExchange httpExchange) throws IOException {
-            final OutputStream responseBody = httpExchange.getResponseBody();
+            resource = getResource(httpExchange.getRequestURI().getQuery());
 
-            httpExchange.sendResponseHeaders(status, response.length());
-
-            responseBody.write(response.getBytes());
-            responseBody.close();
+            try (final OutputStream responseBody = httpExchange.getResponseBody()) {
+                httpExchange.sendResponseHeaders(status, response.length());
+                responseBody.write(response.getBytes());
+            }
         }
 
         public void setResponse(final int status, final String response) {
             this.status = status;
             this.response = response;
         }
-    }
 
+        public String getResource() {
+            return resource;
+        }
+
+        private String getResource(final String query) {
+            final Matcher matcher = RESOURCE_PATTERN.matcher(query);
+
+            if (matcher.matches()) {
+                return matcher.group(1);
+            }
+
+            return "";
+        }
+    }
 }
