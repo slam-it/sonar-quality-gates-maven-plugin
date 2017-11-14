5faad0d532956e6d6dd16cb8ba333d7b10984f52
lammes <Sander.Lammertink@kadaster.nl>
2017-11-14 13:25:08 +0100

    Minor refactoring

--- src/test/java/nl/slam_it/maven/plugin/SonarObjectMapperTest.java
+++ src/test/java/nl/slam_it/maven/plugin/SonarObjectMapperTest.java
@@ -1,31 +1,28 @@
 package nl.slam_it.maven.plugin;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 
-import java.io.IOException;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+@RunWith(MockitoJUnitRunner.class)
 public class SonarObjectMapperTest {
 
     @Mock
     private ObjectMapper objectMapper;
 
+    @InjectMocks
     private SonarObjectMapper sonarObjectMapper;
-
-    @Before
-    public void setUp() {
-        initMocks(this);
-        sonarObjectMapper = new SonarObjectMapper(objectMapper);
-    }
 
     @Test
     public void readValue() throws IOException {
